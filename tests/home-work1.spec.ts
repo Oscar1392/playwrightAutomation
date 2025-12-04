64882dc1c1b1f1b88804fb34237593e8f3429052
askarduishenaliev <d.askar92@gmail.com>
2025-12-03 21:16:10 -0500

    UI form pages

--- tests/home-work1.spec.ts
+++ tests/home-work1.spec.ts
@@ -1,6 +1,6 @@
 import { chromium, expect, test } from '@playwright/test';
 
-test.describe('FORM PAGE', () => { 
+test.describe('HTML FORM PAGE', () => { 
      test('Fill the form and submit', async ({page}) => {
     await page.pause();
     await page.goto('https://testpages.eviltester.com/pages/forms/html-form/');
@@ -21,3 +21,31 @@ test.describe('FORM PAGE', () => {
     await page.pause();
     })
 });
+
+test.describe('DEMO QA FORM PAGE', () => { 
+     test('Fill the form and submit', async ({page}) => {
+    await page.pause();
+    await page.goto('https://demoqa.com/automation-practice-form');
+    await page.pause();    
+    await page.locator('#firstName').fill('oskar');
+    await page.locator('#lastName').fill('test');    
+    await page.locator('#userEmail').fill('test@test.com');
+    const maleLabel = page.locator('label[for="gender-radio-1"]');
+    await maleLabel.scrollIntoViewIfNeeded();
+    await maleLabel.click();
+    await expect(page.locator('#gender-radio-1')).toBeChecked();
+    await page.locator('#userNumber').fill('1234561212');
+    await page.fill('#dateOfBirthInput', '13 Aug 1992');
+    await page.locator('#subjectsInput').fill('test');     
+    await page.setInputFiles('#uploadPicture', 'tests/files/JPG_Test.jpg');
+    await page.locator('#currentAddress').fill('testing text address');     
+    await page.fill('#react-select-3-input', 'Rajasthan');
+    await page.pause();  
+    await page.click('#submit');
+    await page.pause();    
+    await expect(page.getByText('Thanks for submitting the form')).toBeVisible();
+    await page.pause();
+    })
+});
+
+
